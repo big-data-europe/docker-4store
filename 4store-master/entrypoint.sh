70019ccb43a955b5dc0fab78d6f13f14637cea0e
Giannis Mouchakis <gmouchakis@iit.demokritos.gr>
2016-07-19 17:20:06 +0300

    in cluster mode do not auto-start a store

--- 4store-master/entrypoint.sh
+++ 4store-master/entrypoint.sh
@@ -3,6 +3,4 @@
 echo "[4s-boss]" > /etc/4store.conf
 echo "discovery = default" >> /etc/4store.conf
 echo "nodes = $STORE_NODES" >> /etc/4store.conf
-4s-admin create-store default
-4s-admin start-stores -a 
 tail -f /dev/null
